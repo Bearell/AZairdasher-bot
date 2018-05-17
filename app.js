17cbe95d22f69bbb94d095232a88e290a43aed75
Justin Wang <justinxwang@gmail.com>
2018-05-17 01:53:34 -0700

    !burst UAT

--- app.js
+++ app.js
@@ -219,6 +219,83 @@ client.on('message', message => {
         }
     } else
 
+	//burst UAT 
+	if(message.content.startsWith(prefix + "burstuat"))
+	{
+		let prisoner = message.mentions.members.first();
+
+            if(prisoner == null)
+            {
+                message.channel.send("User does not exist");
+                return;
+            }
+			
+			if(prisoner.roles.has("377626418349080595"))
+			{
+				message.channel.send("Upupupu...");
+				return;
+			}
+			
+			if(prisoner.roles.has("374686400307789824"))
+			{
+				message.channel.send("This user is already in UAT!");
+				return;
+			}
+			
+            //message.channel.send("Only admins can send members to UAT");
+			if(!client.devincoin[commander.id])
+			{
+				//if no data exists on the user, add the user with base amount of 10 coins
+				client.devincoin[commander.id] = {
+					devincoins: 10
+				}
+			
+				fs.writeFile("./devincoin.json", JSON.stringify(client.devincoin, null, 4), err => {
+					if(err) throw err;
+				});
+			}
+		
+			if(client.devincoin[commander.id].devincoins < 1){
+				message.channel.send("You've not enough devincoins. Begone, peasant.");
+				return;
+			}
+		
+			if(message.member.roles.has("374686400307789824"))
+			{
+				message.channel.send("Prisoners don't get to UAT people!");
+				return;
+			}
+			//decrease devinco
+			let bursttime = client.devincoin[commander.id].devincoins*4 + 22;
+			client.devincoin[commander.id] = {
+				devincoins: 0)
+			}
+		
+			fs.writeFile("./devincoin.json", JSON.stringify(client.devincoin, null, 4), err => {
+				if(err) throw err;
+				});
+			
+			//create what to add to the json file
+			client.uat[prisoner.id] = {
+				guild: message.guild.id,
+				time: Date.now() + bursttime * 1000
+			}
+			
+			fs.writeFile("./uat.json", JSON.stringify(client.uat, null, 4), err => {
+				if(err) throw err;
+			});
+			
+            prisoner.addRole("374686400307789824").catch(console.error);
+
+            message.channel.send("Sending " + prisoner.displayName + " to UAT");
+            message.channel.send({files: ["./images/uat.jpg"]})
+			.then(msg => {
+				msg.delete(5000)
+			});
+            message.guild.channels.find("name", "uat").send("Welcome to UAT, " + prisoner.displayName + ". You're locked up for... " + bursttime + " seconds!");
+	}
+	else
+	
     //command 5
     if(message.content.startsWith(prefix + "free"))
     {
@@ -570,6 +647,72 @@ client.on('message', message => {
 		unFile = "./images/nu.png";
 		message.channel.send({files: [unFile]});
     }
+	//else
+		
+	//Command Steal Game
+	/*if(message.content.startsWith(prefix + "steal"))
+	{
+	
+	if(args[1] == undefined)
+	{
+		args[1] = 1
+	}
+	if(isNaN(parseInt(args[1])) || args[1] <= 0 || Math.floor(args[1]) == 0)
+	{
+		message.channel.send("Invalid amount.");
+		return;
+	}
+	
+		
+	let stealbet = parseInt(args[1]);
+	let prisoner = message.mentions.members.first();
+	
+	if(prisoner == null)
+            {
+                message.channel.send("User does not exist");
+                return;
+            }
+			
+	var randSteal = Math.floor((Math.random() * 2) + 1);
+	if(client.devincoin[prisoner.id].devincoins < stealbet)
+	{
+		message.channel.send("You can't steal what they don't have!");
+		return;
+	}
+	else if(client.devincoin[commander.id].devincoins < stealbet)
+	{
+		message.channel.send("You can't even afford the risk!");
+		return;
+	}
+	else if(randSteal == 1)
+	{
+		message.channel.send("You successfully stole from");
+		message.mentions.members.first()
+			client.devincoin[commander.id].devincoins += stealbet;
+			client.devincoin[prisoner.id] = {
+				devincoins: client.devincoin[prisoner.id].devincoins - parseInt(args[1])
+			}
+			fs.writeFile("./devincoin.json", JSON.stringify(client.devincoin, null, 4), err => {
+			if(err) throw err;
+			});
+	}
+	else if(randSteal == 0)
+	{
+		message.mentions.members.first()
+		message.channel.send("beat you down and stole your money instead.");
+		client.devincoin[commander.id].devincoins -= stealbet;
+		client.devincoin[prisoner.id] = {
+				devincoins: client.devincoin[prisoner.id].devincoins + parseInt(args[1])
+			}
+			fs.writeFile("./devincoin.json", JSON.stringify(client.devincoin, null, 4), err => {
+			if(err) throw err;
+			});
+	}
+	else
+	{
+		message.channel.send("Hang on! I'm not ready!");
+	}*/
+}
 
 
 
